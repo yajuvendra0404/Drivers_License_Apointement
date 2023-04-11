c204511554f124d0d76f919e1029e98c5e69c63e
yajuvendra <yajuvendra0404@gmail.com>
2023-04-11 10:27:42 -0400

    Module routes added

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"admin",loadChildren:()=>import("@adminModule/admin.module").then(m=>(m.AdminModule))},
+  {path:"examiner",loadChildren:()=> import("@examinerModule/examiner.module").then(m=>(m.ExaminerModule))},
+  {path:"driver",loadChildren:()=> import("@driverModule/driver.module").then(m=>m.DriverModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
